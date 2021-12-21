3af5eff32fbc7114ad23dfd91ee196d3d46c3feb
LMika <lemika.pro@gmail.com>
2021-12-21 09:30:11 +0100

    Initialized commit

---
+++ src/main/java/fr/mika/tunit/TestsUnitairesApplication.java
@@ -0,0 +1,13 @@
+package fr.mika.tunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsUnitairesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsUnitairesApplication.class, args);
+	}
+
+}
